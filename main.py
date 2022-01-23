f765f619e8c99bb70a9b8ac5472e6cfc7916e851
penaduany <94643825+penaduany@users.noreply.github.com>
2022-01-23 17:02:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 """
-Name(s):
-Name of Project:
+Name(s): Duany Pena and Jaivan Gordon
+Name of Project: The Game House
 """
 
 #Write the main part of your program here. Use of the other pages is optional.
+import os
+import time
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
+print(
+    "Welcome to the Game House."
+)  #You can change 'Game House' to whatever you want it to be, I just put it there for now
+print(
+    "If you would like to play Rock Paper Scissors, write Rock Paper Scissors on the next line. If you would like to play Hangman, write Hangman."
+)
+code = input("Which game would you like to play? ")
+if code == 'Rock Paper Scissors':
+    os.system('clear')
+    time.sleep(0.5)
+    import RPS
+elif code == 'Hangman':
+    os.system('clear')
+    time.sleep(0.5)
+    import Hangman
+while code != 'Rock Paper Scissors' and code != 'Hangman' :
+    print("Not a valid game. Try again")
+    time.sleep(1)
+    os.system('clear')
+    print(
+    "If you would like to play Rock Paper Scissors, write Rock Paper Scissors on the next line. If you would like to play Hangman, write Hangman. "
+    )
+    code = input("Which game would you like to play? ")
+    if code == 'Rock Paper Scissors':
+        os.system('clear')
+        time.sleep(0.5)
+        import RPS
+    elif code == 'Hangman':
+        os.system('clear')
+        time.sleep(0.5)
+        import Hangman
+#import page1
+#import page2
 #import page3  # uncomment if you're using page3
 #import page4  # uncomment if you're using page4
